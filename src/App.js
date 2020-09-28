0636bd1793ebd7898550e25db058fb2ee59800e1
Yuriy <Yuriy.Zharenkov@gmail.com>
2020-09-28 13:29:38 +1000

    install redirect, video 68

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import LoginPage from "./components/Login/Login";
 
 
 const App = (props) => {
@@ -33,6 +34,9 @@ const App = (props) => {
 
                 <Route path='/users'
                        render={() => <UsersContainer/>}/>
+
+                <Route path='/login'
+                       render={() => <LoginPage/>}/>
 
                 <Route path='/settings' component={Settings}/>
             </div>
