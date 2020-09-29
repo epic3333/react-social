6248fa0b54f3c39926d851e3938c73d0591eac95
Yuriy <[epic33@mail.ru]>
2020-09-29 11:31:53 +1000

    used setState, local state to change status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
-import Preloader from "../../common/Preloader/Preloader";
+import Preloader from '../../common/Preloader/Preloader'
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
 
@@ -10,11 +11,12 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div>
+            {/*<div>
                 <img src='https://www.wpexplorer.com/wp-content/uploads/wordpress-image-optimization-guide.jpg'></img>
-            </div>
+            </div>*/}
             <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large} alt=""/>ava + description
+                <img src={props.profile.photos.large} alt=""/>
+                <ProfileStatus status={'Hello'}/>
                 <div>Обо мне: {props.profile.aboutMe}</div>
                 <div>Мое имя: {props.profile.fullName}</div>
             </div>
