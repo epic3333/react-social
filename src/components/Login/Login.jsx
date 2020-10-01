2bd401b97f7dc8e1a261d4b752a0375d0bb13b48
[epic3333] <[epic33@mail.ru]>
2020-10-01 17:45:15 +1000

    install redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
+
+const LoginForm = (props) => {
+    return (
+                <form>
+                    <div>
+                        <Field placeholder={'login'} name={'login'} component={'input'}/>
+                    </div>
+                    <div>
+                        <Field placeholder={'password'} name={'password'} component={'input'}/>
+                    </div>
+                    <div>
+                        <Field type={'checkbox'} name={'rememberMe'} component={'input'}/>Remember me
+                    </div>
+                    <div>
+                        <button>Login</button>
+                    </div>
+                </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'}) (LoginForm)
 
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    return  <div>
+                <h1>Login</h1>
+                <LoginReduxForm/>
+            </div>
 }
 
 export default Login;
