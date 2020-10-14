2fe5659f512237f346bfb8fa5a0ad1cbf50d3c2d
epic3333 <epic33@mail.ru>
2020-10-14 19:30:41 +1000

    add new test

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,23 +1,55 @@
-import profileReducer, {addPostActionCreator} from "./profile-reducer";
+import profileReducer, {addPostActionCreator, deletePost} from "./profile-reducer";
 import React from "react";
 
-
+let state = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 15},
+        {id: 2, message: 'Im fine', likesCount: 30},
+        {id: 3, message: 'blabla', likesCount: 20},
+        {id: 4, message: 'dada', likesCount: 10}
+    ]
+};
 
 test('reducer test new post added', () => {
     //1. test start data
     let action = addPostActionCreator('newPostText');
-    let state = {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 15},
-            {id: 2, message: 'Im fine', likesCount: 30},
-            {id: 3, message: 'blabla', likesCount: 20},
-            {id: 4, message: 'dada', likesCount: 10}
-        ]
-    };
+
     //2. action
     let newState = profileReducer(state, action);
 
     //3. expectation
     expect (newState.posts.length).toBe(5);
+});
+
+test('message should be correct', () => {
+    //1. test start data
+    let action = addPostActionCreator('newPostText');
+
+    //2. action
+    let newState = profileReducer(state, action);
+
+    //3. expectation
     expect (newState.posts[4].message).toBe('newPostText');
+});
+
+test('after deleting length of messagess should be decrement', () => {
+    //1. test start data
+    let action = deletePost(1);
+
+    //2. action
+    let newState = profileReducer(state, action);
+
+    //3. expectation
+    expect (newState.posts.length).toBe(3);
+});
+
+test(`after deleting shouldn't be decrement if id is incorrect`, () => {
+    //1. test start data
+    let action = deletePost(1000);
+
+    //2. action
+    let newState = profileReducer(state, action);
+
+    //3. expectation
+    expect (newState.posts.length).toBe(4);
 });
