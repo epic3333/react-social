ccb74215969ea555f4e506e5af4b6b76ca01c43e
epic3333 <epic33@mail.ru>
2020-10-18 14:02:45 +1000

    follow, unfollow refactor

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -139,17 +139,13 @@ const followUnfollowFlow = async (dispatch, userId, apiMethod, actionCreator) =>
 
 export const follow = (userId) => {
     return async (dispatch) => {
-        let apiMethod = usersAPI.follow.bind(usersAPI);
-        let actionCreator = followSuccess;
-        followUnfollowFlow(dispatch, userId, apiMethod, actionCreator);
+        followUnfollowFlow(dispatch, userId, usersAPI.follow.bind(usersAPI), followSuccess);
     }
 }
 
 export const unfollow = (userId) => {
     return async (dispatch) => {
-        let apiMethod = usersAPI.unfollow.bind(usersAPI);
-        let actionCreator = unfollowSuccess;
-        followUnfollowFlow(dispatch, userId, apiMethod, actionCreator);
+        followUnfollowFlow(dispatch, userId, usersAPI.unfollow.bind(usersAPI), unfollowSuccess);
     }
 }
 
